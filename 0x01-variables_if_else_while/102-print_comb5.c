fc493016949763060887343b011c4324c047f840
overwatch <nzioki.grace@gmail.com>
2022-04-08 20:27:19 +0000

    combinations

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+  * main - prints loads of numbers
+  * Return: 0
+  */
+int main(void)
+{
+	int a, b, c, d;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			for (c = 0; c <=9; c++)
+			{
+				for (d = 0; d <= 9; d++)
+				{
+					int first = a * 10 + b;
+					int last = c * 10 + d;
+
+					if (first < last)
+					{
+						putchar('0' + a);
+						putchar('0' + b);
+						putchar(' ');
+						putchar('0' + c);
+						putchar('0' + d);
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
