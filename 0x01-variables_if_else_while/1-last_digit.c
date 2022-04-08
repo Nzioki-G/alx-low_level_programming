b7690d4f2ee69e9959f093891bf60769d8ef6b42
overwatch <nzioki.grace@gmail.com>
2022-04-08 13:09:27 +0000

    using %

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,18 +9,19 @@
 int main(void)
 {
 	int n;
+	int last_digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	char str[] = "Last digit of";
-	int last_digit = n % 10;
+	last_digit = n % 10;
 
 	if (last_digit > 5)
-		printf("%s %d is %d and is greater than 5\n", str, n, last_digit);
+		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
 	else if (last_digit == 0)
-		 printf("%s %d is %d and is 0\n", str, n, last_digit);
+		printf("Last digit of %d is %d and is 0\n", n, last_digit);
 	else if (last_digit < 6 && last_digit != 0)
-		printf("%s %d is %d and is less than 6 and not 0\n", str, n, last_digit);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n",
+n, last_digit);
 
 	return (0);
 }
