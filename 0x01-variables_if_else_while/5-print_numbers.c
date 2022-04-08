792a1493d97eaf9e291438c99e7d3e4470cb1f8b
overwatch <nzioki.grace@gmail.com>
2022-04-08 13:47:46 +0000

    print 0-10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints 0-10
+ * Return: 0
+ */
+int main(void)
+{
+	int num = 0;
+
+	for (; num <= 10; num++)
+		printf("%d", num);
+	printf("\n");
+	return (0);
+}
