2b6b46127e13184130f6f69bfc980860bc7cf7ce
overwatch <nzioki.grace@gmail.com>
2022-04-12 21:55:25 +0000

    first 98 fibonacci nums

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,18 +7,18 @@
 int main(void)
 {
 	/* @pp: F(n-2), @prev: F(n-1), @fib: F(n) */
-	unsigned long int pp = 0, prev = 1, fib = 0;
+	long double pp = 0, prev = 1, fib = 0;
 	int counter = 0;
 
-	while (counter <= 98)
+	while (counter < 98)
 	{
 		/* get @fib, set @pp & @prev */
 		fib = pp + prev;
 		pp = prev;
 		prev = fib;
-		printf("%lu", fib);
-		if (counter < 97)
-			printf(", ");
+		printf("%.0Lf, ", fib);
+/*		if (counter < 97)
+			printf(", "); */
 		counter++;
 	}
 	printf("\n");
