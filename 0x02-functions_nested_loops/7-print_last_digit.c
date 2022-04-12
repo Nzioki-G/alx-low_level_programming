08b9d8e480e953eb0b3266d7928755445f31441c
overwatch <nzioki.grace@gmail.com>
2022-04-12 15:49:34 +0000

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,14 +1,25 @@
 #include "main.h"
 
 /**
+  * _abs - gets the the absolute value of number
+  * @num: the integer to be absoluted
+  * Return: absolute val
+  */
+int _abs(int num)
+{
+	if (num >= 0)
+		return (num);
+	else
+		return (num * -1);
+}
+
+/**
   * print_last_digit - returns last digit
   * @num: number under consideration
   * Return: value of last digit
   */
 int print_last_digit(int num)
 {
-	if (num < 0)
-		num *= -1;
-	_putchar('0' + (num % 10));
-	return (0);
+	_putchar('0' + (_abs(num) % 10));
+	return (_abs(num) % 10);
 }
