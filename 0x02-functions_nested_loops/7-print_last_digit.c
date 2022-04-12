df91a90b86edb51fdf31df27d3e3caf3096984ff
overwatch <nzioki.grace@gmail.com>
2022-04-12 15:01:41 +0000

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+  * print_last_digit - returns last digit
+  * @num: number under consideration
+  * Return: value of last digit
+  */
+int print_last_digit(int num)
+{
+	return (num % 10);
+}
