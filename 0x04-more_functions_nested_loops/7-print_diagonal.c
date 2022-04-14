3df56247d9e429807babb6744e9e1d4b45bf3908
overwatch <nzioki.grace@gmail.com>
2022-04-14 09:45:54 +0000

    print diagnal line using \

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * print_diagonal - prints a diagnal line
+  * @n: length of line \..n
+  * Return: void
+  */
+void print_diagonal(int n)
+{
+	int count;
+
+	if (n <= 0)
+		_putchar('\n');
+
+	for (count = 1; count <= n; count++)
+	{
+		int space;
+
+		for (space = 1; space <= count; space++)
+			_putchar(' ');
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
