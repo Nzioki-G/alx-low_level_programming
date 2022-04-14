d3b8e32f8c7d3b031484ee1a330a01a4a1e98b5e
overwatch <nzioki.grace@gmail.com>
2022-04-14 08:38:38 +0000

    printing 0-9 sans 2,4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * print_most_numbers - prints 0-9 excluding 2&4
+  * Return: void
+  */
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		if (i == 2 || i == 4)
+		{
+			i++;
+			continue;
+		}
+		_putchar(i + '0');
+		i++;
+	}
+	_putchar('\n');
+}
