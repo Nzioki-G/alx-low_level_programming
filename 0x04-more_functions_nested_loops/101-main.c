4d7a9f4e8eb0b16657d57250a6eeaa08877d22c4
overwatch <nzioki.grace@gmail.com>
2022-04-14 17:11:35 +0000

    print numbers using _putchar

---
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_number(98);
+    _putchar('\n');
+    print_number(402);
+    _putchar('\n');
+    printf("==>%d\n", INT_MAX);
+    print_number(INT_MAX);
+    _putchar('\n');
+    print_number(-1);
+    _putchar('\n');
+    print_number(-98);
+    _putchar('\n');
+    print_number(INT_MIN);
+    _putchar('\n');
+    printf("==>%d\n", INT_MIN);
+    return (0);
+}
