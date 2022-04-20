7640c033e0511d6bd82fc2e5bd24a650a5f3dae7
overwatch <nzioki.grace@gmail.com>
2022-04-20 09:46:53 +0000

    using pointers to swap 2 values

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,8 +2,8 @@
 
 /**
   * swap_int - swaps to integer values
-  * @*a: pointer to first value
-  * @*b: pointer to second value
+  * @a: pointer to first value
+  * @b: pointer to second value
   * Return: void
   */
 void swap_int(int *a, int *b)
