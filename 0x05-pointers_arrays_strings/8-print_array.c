0a442ab0c47ce5f37799c49b18ec01729c8ed2e2
overwatch <nzioki.grace@gmail.com>
2022-04-20 17:47:34 +0000

    prints n elements of an array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * print_array - print n elements of array
+  * @a: pointer to the array
+  * @n: number of elements
+  * Return: void
+  */
+void print_array(int *a, int n)
+{
+	int i;
+
+	/* loop to n-1 (for separating) */
+	for (i = 0; i < (n - 1); i++)
+		printf("%d, ", a[i]);
+
+	/* print nth */
+	printf("%d\n", a[i]);
+}
