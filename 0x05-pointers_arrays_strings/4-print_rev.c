907393dbbb5aec9653e97909f06ef65d5cb3763c
overwatch <nzioki.grace@gmail.com>
2022-04-20 17:42:09 +0000

    prints a string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * print_rev - prints a string in reverse
+  * @s: the string under consideration
+  * Return: void
+  */
+void print_rev(char *s)
+{
+	int count = 0;
+
+	/* get the strlen */
+	while (s[count])
+		count++;
+
+	/* print from end to index 0 */
+	while (count > 0)
+	{
+		_putchar(s[count-1]);
+		count--;
+	}
+	_putchar('\n');
+}
