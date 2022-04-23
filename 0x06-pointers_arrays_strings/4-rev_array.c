160b20b4c0166ef4daf4e1902f49b64086b4581e
overwatch <nzioki.grace@gmail.com>
2022-04-23 07:50:50 +0000

    reverse an array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * reverse_array - reverses the contents of an array
+  * @a: the array in question
+  * n: number of elements in a
+  * Return: void
+  */
+void reverse_array(int *a, int n)
+{
+	int i, midpoint, temp;
+
+	/* set midpoint */
+	midpoint = n / 2;
+
+	/* loop through to mid of array */
+	for (i = 0; i < midpoint; i++)
+	{
+		/* swap the values at index "i" and "(n-1)-i" */
+		temp = a[i];
+		a[i] = a[n-(i+1)];
+		a[n-(i+1)] = temp;
+	}
+}
