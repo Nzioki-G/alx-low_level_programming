89fa912ee6bd94a0beeaf5f5078b720026cb187c
overwatch <nzioki.grace@gmail.com>
2022-04-27 20:29:17 +0000

    get length of a PREFIX substring

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,22 +8,25 @@
   */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i = 0, j, length = 0;
+	unsigned int i = 0, j, length = 0, old_length;
 
-	/* loop to end of s */
+	/* for each char in s */
 	while (s[i])
 	{
+		old_length = length;
 		for (j = 0; accept[j]; j++)
 		{
+			/* check for match */
 			if (s[i] == accept[j])
 			{
 				length++;
-				i++;
 			}
-			else
-				return (length);
 		}
+		/* if nothing matched, break */
+		if (length == old_length)
+			break;
 		i++;
 	}
+	
 	return (length);
 }
