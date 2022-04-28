61563b7967b2760d91ba5ba1ae7599f1ec91f5e0
overwatch <nzioki.grace@gmail.com>
2022-04-28 16:58:52 +0000

    checks whether a number is prime or not

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * isPrime - checks if num is prime
+  * @num: the nimber
+  * @i: counter
+  * Return: 0-not prime, 1-prime
+  */
+int isPrime(int num, int i)
+{
+	/* base cases: 1, 2, and not-prime */
+	if (abs(num) == 1)
+		return (0);
+	if (abs(num) == 2)
+		return (1);
+	if (num % i == 0)
+		return (0);
+
+	/* when it's a prime, i^2>num */
+	if (num < (i * i))
+		return (1);
+	return (isPrime(num, i + 1));
+}
+
+/**
+  * is_prime_number - checks for prime numbers
+  * @n: number under consideration
+  * Return: 1 if prime else 0
+  */
+int is_prime_number(int n)
+{
+	/* call a fn that has a static variable divisor */
+	return (isPrime(n, 2));
+}
