4e2dd311d0110b63cb70f44c76767fbe2f7835d5
overwatch <nzioki.grace@gmail.com>
2022-04-28 09:23:38 +0000

    print string with putchar using recusrion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * _puts_recursion - prints a string using recursion
+  * @s: string to print
+  * Retuen: nothing
+  */
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	/* if null */
+	if (!s[i])
+	{
+		putchar('\n');
+		return;
+	}
+
+	/* print 1st character */
+	_putchar(s[i]);
+
+	/* call this fn again with string from 2nd char */
+	_puts_recursion(&s[i+1]);
+}
