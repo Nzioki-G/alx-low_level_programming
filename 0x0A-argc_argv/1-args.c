b63abac2ee376e18a52bb52a8fce3feb76a3488f
overwatch <nzioki.grace@gmail.com>
2022-05-03 18:30:22 +0000

    print number of arguements passed at execution

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,7 +6,7 @@
   * @argv: array of arguements passed during execution
   * Return: 0.
   */
-int main(int argc, char* argv[])
+int main(int argc, __attribute__((unused)) char* argv[])
 {
 	printf("%d\n", argc - 1);
 	return (0);
