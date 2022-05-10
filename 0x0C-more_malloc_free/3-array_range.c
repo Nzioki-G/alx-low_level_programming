8b6959845c4f7cc7a414135dfc96ffbbdced3c9f
overwatch <nzioki.grace@gmail.com>
2022-05-10 21:11:15 +0000

    m-alloc for variable length arrays

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * array_range - creates an array of integers
+  * @min: the starting point
+  * @max: the end point
+  * Return: pointer to this array
+  */
+int *array_range(int min, int max)
+{
+	int *ar, i, size;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+
+	/* allocate mem for ar */
+	size = max - min + 1;
+	ar = malloc(sizeof(int) * size);
+
+	if (!ar)
+	{
+		return (NULL);
+	}
+
+	/* populate the array */
+	for (i = 0; i < size; i++)
+	{
+		ar[i] = i;
+	}
+	return (ar);
+}
