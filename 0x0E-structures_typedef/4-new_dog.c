aceaf8bdd2ba13020c86a1b84293c1f79e92638d
overwatch <nzioki.grace@gmail.com>
2022-05-12 20:08:43 +0000

    create a new struct onject

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -6,17 +6,23 @@
   * @name: dog's name
   * @age: dog's age
   * @owner: dog's owner
+  * Return: pointer to new dog if successful otherwise NULL
   */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *doggy;
+	char *nm, *own;
+
+	nm = name;
+	own = owner;
+
 	doggy = malloc(sizeof(dog_t));
 
 	if (doggy == NULL)
 		return (NULL);
 
-	doggy->name = name;
+	doggy->name = nm;
 	doggy->age = age;
-	doggy->owner = owner;
+	doggy->owner = own;
 	return (doggy);
 }
