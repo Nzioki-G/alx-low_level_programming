9d2ed62cd8920decb9d94745f4aaf5667ee2192c
overwatch <nzioki.grace@gmail.com>
2022-05-12 18:58:08 +0000

    print a struct

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,26 +8,25 @@
   */
 void print_dog(struct dog *d)
 {
-	if (d)
-	{
-		printf("Name: ");
-		if (d->name)
-			printf("%s\n", d->name);
-		else
-			printf("(nil)\n");
-
-		printf("Age: ");
-		if (d->age)
-			printf("%f\n", d->age);
-		else
-			printf("(nil)\n");
-
-		printf("Owner: ");
-		if (d->owner)
-			printf("%s\n", d->owner);
-		else
-			printf("(nil)\n");
-	}
-	else
+	if (d == NULL)
 		return;
+
+	printf("Name: ");
+	if (d->name)
+		printf("%s\n", d->name);
+	else
+		printf("(nil)\n");
+
+	printf("Age: ");
+	if (d->age)
+		printf("%f\n", d->age);
+	else
+		printf("(nil)\n");
+
+	printf("Owner: ");
+	if (d->owner)
+		printf("%s\n", d->owner);
+	else
+		printf("(nil)\n");
+
 }
