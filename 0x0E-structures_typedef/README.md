f08b52f928c42bce13b6c651655b18aa4713a860
overwatch <nzioki.grace@gmail.com>
2022-05-12 17:31:36 +0000

    dog poppy

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,3 @@
+Structures and typedef
+Defining, initializing and calling structs
+Use typedef to shorten declarations
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_POPPY
+#define DOG_POPPY
+
+/**
+  * struct dog - a structure rep for a simple dog
+  * @name: dog's name
+  * @age: dog's age
+  * @owner: dog's owner
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
