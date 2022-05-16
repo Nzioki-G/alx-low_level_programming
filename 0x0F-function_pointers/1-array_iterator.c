2af722aef584b7a2d475a9c0cd388d7c25862d25
overwatch <nzioki.grace@gmail.com>
2022-05-16 20:07:46 +0000

    call funtion on array elements

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+  * array_iterator - executes ACTION on ech elemnt of ARRAY
+  * @array: array of elemnts to pass to ACTION
+  * @size: length of ARRAY
+  * @action: function to call
+  * Return: nothing
+  */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	long unsigned int i = 0;
+
+	if (!action || !array)
+		return;
+
+	while (i < size)
+	{
+		action(array[i]);
+		i++;
+	}
+}
