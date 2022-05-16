dd65455e976f039c2ee2f3461de0116d0f79ef3b
overwatch <nzioki.grace@gmail.com>
2022-05-16 20:45:57 +0000

    search for an int in array using

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,31 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+  * int_index - searches for integer
+  * @array: integers to search through
+  * @size: size of ARRAY
+  * @cmp: pointer to function that compares values
+  * Return: index of first match
+  */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0, diff;
+
+	if (size <= 0)
+		return (-1);
+
+	while (i < size)
+	{
+		diff = cmp(array[i]);
+
+		/* if cpm returns a positive */
+		if (diff)
+			return (i);
+
+		i++;
+	}
+
+	/* no match found */
+	return (-1);
+}
