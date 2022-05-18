2787fef2fedceaafb2ff0efd0124547b56630288
grace <nzioki.grace@gmail.com>
2022-05-18 16:03:10 -0700

    print rand num of strings

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+
+/**
+* print_strings - prints varid number of strings
+* @separator: string tha separates strings printed
+* @n: number of args to expect
+* @...: the strings to print
+* Return: nothing
+*/
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *str;
+	va_list(args);
+
+	va_start(args, n);
+	
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(args, char *);
+
+		if (str)
+			printf("%s", str);
+		else
+			printf("(nil)");
+		if (separator && i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+
+	va_end(args);
+}
