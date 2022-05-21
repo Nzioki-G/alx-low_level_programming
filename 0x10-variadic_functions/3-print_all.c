f8dbebf98459cc3b3c572b464a18c0811f511168
grace <nzioki.grace@gmail.com>
2022-05-21 07:09:20 -0700

    print different types of args passed

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,44 @@
+#include "variadic_functions.h"
+
+/**
+  * print_all - prints anything
+  * @format: a list of types of args passed
+  * @...: the other args passed
+  *
+  * Return: nothing
+  */
+void print_all(const char * const format, ...)
+{
+	int i = 0;
+	char c;
+
+	va_list(args);
+
+	va_start(args, format);
+	while (format[i])
+	{
+		c = format[i];
+		switch (c)
+		{
+			case 99:
+				printf("%c", va_arg(args, int));
+				break;
+			case 102:
+				printf("%f", va_arg(args, double));
+				break;
+			case 105:
+				printf("%d", va_arg(args, int));
+				break;
+			case 115:
+				printf("%s", va_arg(args, char *));
+				break;
+			default:
+				break;
+		}
+		if ((c == 99 || c == 102 || c == 105 || c == 115) && format[i + 1])
+			printf(", ");
+		i++;
+	}
+	printf("\n");
+	va_end(args);
+}
