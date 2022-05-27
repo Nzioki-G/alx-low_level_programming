0756af816d21fa0fe208865ccd593b0488227989
overwatch <nzioki.grace@gmail.com>
2022-05-27 10:09:16 +0300

    pop element from list

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -14,16 +14,34 @@ int pop_listint(listint_t **head)
 	if (!*head)
 		return (0);
 
-	temp = *head; /* hold first */
-	*head = temp->next; /* set 2nd as first */
+	/*
+	 * first => *head == temp
+	 * 1st val => temp->n
+	 * second=> temp->next
+	 * temp = temp->next;
+	 * second => temp
+	 * free(*head)
+	 * head = temp
+	 */
+	temp = *head;
+	val = temp->n;
+	temp = temp->next;
+	free(*head);
+	*head = temp;
+	return (val);
 
-	/* if the next val exists */
-	if (*head)
-	{
-		val = temp->next->n;
-		free(temp);
-		return (val);
-	}
-	else
-		return (0);
+	/*temp = *head; * hold first */
+	/**head = temp->next; * set 2nd as first */
+
+	/*
+	* if the next val exists
+	* if (*head)
+	* {
+	*	val = temp->next->n;
+	*	free(temp);
+	*	return (val);
+	* }
+	* else
+	*	return (0);
+	*/
 }
