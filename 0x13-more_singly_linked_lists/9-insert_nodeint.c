2d32e3f0a5bf15bbffe7dd7787430a863670cb31
overwatch <nzioki.grace@gmail.com>
2022-05-27 12:37:54 +0300

    insert node at index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,7 +23,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		current_node = *head;
 		while (current_node)
 		{
-			if (i == idx)
+			if (i + 1 == idx)
 			{
 				/*
 				 * link to previous node => current
