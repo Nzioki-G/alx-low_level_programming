0cf747800e755157d0cab33b3283f3b10fe04e8b
overwatch <nzioki.grace@gmail.com>
2022-05-30 22:07:41 +0300

    print maybe-looped-list

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,56 @@
+#include "lists.h"
+
+/**
+ * removeloop - removes a loop in a list
+ * @first: head of the list
+ * @loopy: point in list where there is a loop
+ * Return: nothing
+ */
+listint_t *removeloop(listint_t *first, listint_t *loopy)
+{
+	listint_t *ptr = loopy;
+
+	while (1) /* no condition needed as we'll break out */
+	{
+		while (ptr->next != first && ptr->next != loopy)
+		{
+			ptr = ptr->next;
+		}
+		if (ptr->next == first)
+			break;
+
+		first = first->next;
+		ptr = loopy;
+	}
+	/* we have the two nodes that form a loop: break it */
+	ptr->next = NULL;
+	return (first);
+}
+
+/**
+ * print_listint_safe - prints a list (might be looped: a node points
+ * to a previously visited node)
+ * @head: the first item on the list
+ *
+ * Return: number of nodes in the list
+ */
+size_t print_listint_safe(listint_t *head)
+{
+	int i = 0;
+	listint_t *slow = head, *fast = head;
+
+	while (slow && fast && fast->next)
+	{
+		printf("[%p] [%d]\n", (void *)slow, slow->n);
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+		{
+			/* we have a loop */
+			slow = removeloop(head, slow);
+		}
+		i++;
+	}
+
+	return (i);
+}
