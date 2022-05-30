08e08e878743778fa27b64ae00a96e0e9c4f1da5
overwatch <nzioki.grace@gmail.com>
2022-05-31 00:55:09 +0300

    print linked list maybe-loop

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,32 +1,6 @@
 #include "lists.h"
 
 /**
- * removeloop - removes a loop in a list
- * @first: head of the list
- * @loopy: point in list where there is a loop
- * Return: nothing
- */
-void removeloop(const listint_t *first, listint_t *loopy)
-{
-	listint_t *ptr = loopy;
-
-	while (1) /* no condition needed as we'll break out */
-	{
-		while (ptr->next != first && ptr->next != loopy)
-		{
-			ptr = ptr->next;
-		}
-		if (ptr->next == first)
-			break;
-
-		first = first->next;
-		ptr = loopy;
-	}
-	/* we have the two nodes that form a loop: break it */
-	ptr->next = NULL;
-}
-
-/**
  * print_listint_safe - prints a list (might be looped: a node points
  * to a previously visited node)
  * @head: the first item on the list
@@ -36,21 +10,41 @@ void removeloop(const listint_t *first, listint_t *loopy)
 size_t print_listint_safe(const listint_t *head)
 {
 	int i = 0;
-	listint_t *slow, *fast;
+	listint_t *slow = head->next, *fast = head->next;
 
-	while (head)
+	if (!head)
+		return (0);
+	printf("[%p] %d\n", (void *)head, head->n); /* print 1st */
+	while (slow && fast && fast->next)
 	{
-		printf("[%p] %d\n", (void *)head, head->n);
-		slow = head->next;
-		fast = head->next->next;
-		if (slow == fast)
+		printf("[%p] %d\n", (void *)slow, slow->n);
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast) /* clash point */
 		{
 			/* we have a loop */
-			removeloop(head, slow);
+			while (1)
+			{
+				/* if we get to beginning of loop */
+				if (slow->next == head)
+				{
+					printf("[%p] %d\n", (void *)slow, slow->n);
+					slow->next = NULL;
+					break;
+				}
+				while (slow->next != fast && slow->next != head)
+					slow = slow->next;
+				head = head->next;/* increment head */
+				slow = fast; /* reset slow to clash point */
+			}
 		}
-		head = head->next;
 		i++;
 	}
-	printf("i=%d\n", i);
+	while (slow) /* if no loop, then we exited before end of list */
+	{
+		printf("[%p] %d\n", (void *)slow, slow->n);
+		slow = slow->next;
+		i++;
+	}
 	return (i);
 }
