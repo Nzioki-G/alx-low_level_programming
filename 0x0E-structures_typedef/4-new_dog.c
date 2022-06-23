3fa7e7bb810479a54183db2dd12a696769afeffc
overwatch <nzioki.grace@gmail.com>
2022-06-23 16:00:54 +0300

    new structure object

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 /**
-  * new_dog - cretes a new instance of struct dog
+  * new_dog - creates a new instance of struct dog
   * @name: dog's name
   * @age: dog's age
   * @owner: dog's owner
@@ -12,18 +12,34 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
-	char *nm, *own;
+
+	if (name == NULL || owner == NULL)
+	{
+		return (NULL);
+	}
 
 	d = malloc(sizeof(dog_t));
+	if (d)
+	{
+		d->name = strdup(name); /* set name */
+		if (d->name == NULL)
+		{
+			free(d->name);
+			free(d);
+			return (NULL);
+		}
+		d->age = age; /* set age */
+		d->owner = strdup(owner); /* set owner */
+		if (d->owner == NULL)
+		{
+			free(d->name);
+			free(d->owner);
+			free(d);
+			return (NULL);
+		}
+		return (d);
+	}
 
-	if (d == NULL)
-		return (NULL);
-
-	nm = (name) ? strdup(name) : NULL;
-	own = (owner) ? strdup(owner) : NULL;
-
-	d->name = nm;
-	d->age = age;
-	d->owner = own;
-	return (d);
+	free(d);
+	return (NULL);
 }
