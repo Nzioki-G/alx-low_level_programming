5401f065df4c68a22c42da8046563299eace2fa9
overwatch <nzioki.grace@gmail.com>
2022-07-07 20:20:32 +0300

    insert t head

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - inserts a node @ head of list
+ * @head: firt node of list
+ * @n: the data value of node
+ *
+ * Return: pointer to element added, NULL if fail
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
+
+	new_node = malloc(sizeof(dlistint_t));
+
+	if (!new_node)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = NULL;
+
+	if (*head)
+	{
+		new_node->next = *head;
+		(*head)->prev = new_node;
+	}
+
+	*head = new_node;	
+
+	return (*head);
+}
