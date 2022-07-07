36cd3169a0f8a3e7f5033e325edf97f8890908df
overwatch <nzioki.grace@gmail.com>
2022-07-08 00:37:44 +0300

    insert node at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -5,12 +5,11 @@
  * @h: head of list
  * @idx: position to insert
  * @n: data for new node
- *
  * Return: address of new node or NULL on failure
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *current, *new;
+	dlistint_t *current = *h, *new;
 	unsigned int i = 0;
 
 	new = malloc(sizeof(dlistint_t));
@@ -18,36 +17,36 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	new->n = n;
 
-	if (idx == 0) /* if list is empty */
+	/* insert at head */
+	if (idx == 0)
 	{
 		new->next = *h;
 		(*h)->prev = *h ? new : NULL;
 		*h = new;
 		return (*h);
 	}
-	current = *h;
+	/* insert in the middle */
 	while (current->next)
 	{
 		current = current->next;
 		i++;
 		if (i == idx)
 		{
-			new->prev = current->prev;
-			new->next = current;
-			current->prev->next = new;
-			current->prev = new;
+			new->prev = current->prev; /* prev <- new */
+			current->prev->next = new; /* prev -> new */
+			new->next = current; /* new -> current */
+			current->prev = new; /* new <- current */
+			return (*h);
 		}
 	}
-	if (i + 1 < idx) /* out of bounds */
-	{
-		free(new);
-		return (NULL);
-	}
-
-	if (!current->next && i + 1 == idx) /* insert at tail */
+	/* insert at tail */
+	if (!current->next && i + 1 == idx)
 	{
 		current->next = new;
 		new->prev = current;
+		return (*h);
 	}
-	return (*h);
+
+	free(new);	/* out of bounds */
+	return (NULL);
 }
