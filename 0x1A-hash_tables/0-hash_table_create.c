f427d36336edae95299cfe9a6462be59e9c8ff33
overwatch <nzioki.grace@gmail.com>
2022-07-29 01:10:07 +0300

    create a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,22 +8,38 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	/* create an array of nodes */
+	/* create an array for nodes */
 	hash_node_t **array;
 	hash_table_t *new_ht;
+	unsigned long int i = 0;
 
 	/* allocate memo for the array */
-	array = malloc(size);
+	array = malloc(size * sizeof(hash_node_t));
 	if (!array)
 		return (NULL);
 
 	/* allocate mem for hash table */
 	new_ht = malloc(sizeof(hash_table_t));
 	if (!new_ht)
+	{
+		free(array);
 		return (NULL);
+	}
 
 	/* init the ht */
 	new_ht->size = size;
 	new_ht->array = array;
+	if (!new_ht)
+	{
+		free(new_ht->array);
+		free(new_ht);
+		return (NULL);
+	}
+	/* once we've created the ht, init its array */
+	while (i < size)
+	{
+		array[i] = NULL;
+		i++;
+	}
 	return (new_ht);
 }
