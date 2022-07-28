cfb9fea7d76d10514a865f961e86cb4f7c3a49f6
overwatch <nzioki.grace@gmail.com>
2022-07-29 00:33:19 +0300

    create a hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table of given size
+ * @size: the size of the array ht is built upon
+ *
+ * Return: HT on success, NULL on failure
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	/* create an array of nodes */
+	hash_node_t **array;
+	hash_table_t *new_ht;
+
+	/* allocate memo for the array */
+	array = malloc(size);
+	if (!array)
+		return NULL;
+
+	/* allocate mem for hash table */
+	new_ht = malloc(sizeof(hash_table_t));
+	if (!new_ht)
+		return NULL;
+
+	/* init the ht */
+	new_ht->size = size;
+	new_ht->array = array;
+	return (new_ht);
+}
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,6 @@
+## Hash Tables
+Learn about:
+> Hash function and what makes a good one
+> Hash table, how it works, how to use
+> Collision? How to deal with them
+> Most common uses of Hash tables?
