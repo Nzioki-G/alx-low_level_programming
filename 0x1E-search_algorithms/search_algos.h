c9db1c112603b5a2a6192ff0b7059c9ce14eb071
overwatch <nzioki.grace@gmail.com>
2022-11-01 20:01:02 +0300

    headers

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,9 +4,11 @@
 #include <stdio.h>
 #include <stddef.h>
 #include <stdlib.h>
+#include <math.h>
 
 int linear_search(int *array, size_t size, int value);
 void print_arr(int *arr, size_t start_idx, size_t end_idx);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
 
 #endif
