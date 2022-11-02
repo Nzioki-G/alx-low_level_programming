dae8629b3b7f5bee10d0a36409c5a841f56b51e5
overwatch <nzioki.grace@gmail.com>
2022-11-02 22:33:32 +0300

    headers

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,11 +6,29 @@
 #include <stdlib.h>
 #include <math.h>
 
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+	int n;
+	size_t index;
+	struct listint_s *next;
+} listint_t;
+
 int linear_search(int *array, size_t size, int value);
 void print_arr(int *arr, size_t start_idx, size_t end_idx);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
 
 #endif
