111365b8b325a3eb422a39d88b0a2f8235ff5913
overwatch <nzioki.grace@gmail.com>
2022-11-02 21:54:47 +0300

    headers

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -11,5 +11,6 @@ void print_arr(int *arr, size_t start_idx, size_t end_idx);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
 
 #endif
