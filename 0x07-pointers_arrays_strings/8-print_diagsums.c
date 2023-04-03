8d5b7c4c61e5fb0a139dfb02a0d1940d264ea436
overwatch <nzioki.grace@gmail.com>
2023-04-03 16:37:21 +0300

    add diagonals

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -20,36 +20,4 @@ void print_diagsums(int *a, int size)
 	}
 
 	printf("%d, %d\n", right_sum, left_sum);
-
-	/* int row, col, index = 0, sum_left = 0, sum_right = 0; */
-
-	/* for (row = 0; row < size; row++)
-	{
-		for (col = 0; col < size; col++)
-		{
-			if (row == col)
-			{
-				sum_left += a[index];
-			}
-			index++;
-		}
-	}
-	printf("%d, ", sum_left);
-
-	index = 0;
-	for (row = 0; row < size; row++)
-	{
-		for (col = 0; col < size; col++)
-		{
-
-			if (row + col == (size - 1))
-			{
-				sum_right += a[index];
-			}
-			index++;
-		}
-	}
-
-	printf("%d\n", sum_right);
-	*/
 }
