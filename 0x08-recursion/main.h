a977a7c47f4086430fd8ec73a8ee99c8c187e1cc
overwatch <nzioki.grace@gmail.com>
2023-04-06 14:06:40 +0300

    headers

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int _putchar(char c);
 void _puts_recursion(char *s);
@@ -6,5 +7,9 @@ void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
-int _sqr_recursion(int n);
+int _sqrt_recursion(int n);
+void sqrt_helper(int num, int potentialRoot);
 int is_prime_number(int n);
+int is_palindrome(char *s);
+int palindrome_helper(char *s, int last_index);
+int wildcmp(char *s1, char *s2);
