97bb6b5706a76f05b4f6b8771f30ddccf8719b00
overwatch <nzioki.grace@gmail.com>
2023-04-12 10:27:44 +0300

    concat all command line args

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * argstostr - concatenates all the arguments of your program
+ * @ac: number of args
+ * @av: array of args
+ * Return: pointer to new str or NULL on fail
+ */
+char *argstostr(int ac, char **av)
+{
+	char *args_string;
+	int i, len = 0, new = 0, arg;
+	unsigned long int j;
+
+	if (ac == 0 || !av)
+		return (NULL);
+
+	/* get total length of args */
+	for (i = 0; i < ac; i++)
+		len += strlen(av[i]);
+	len += (2 * ac);
+	args_string = malloc(len * sizeof(char));
+
+	for (arg = 0; arg < ac; arg++)
+	{
+		for (j = 0; j < strlen(av[arg]); j++)
+		{
+			args_string[new] = av[arg][j];
+			new++;
+		}
+		/* add new line */
+		args_string[new] = '\n';
+		new++;
+	}
+	args_string[new] = '\0';
+	return (args_string);
+}
+
+
