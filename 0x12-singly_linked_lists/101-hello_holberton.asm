7f71a26ec1c53f9fcb46ed7d83cb39873d50c7b8
overwatch <nzioki.grace@gmail.com>
2023-04-28 00:47:12 +0300

    print hello on asm!

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -2,19 +2,19 @@ section .data
     hi db 'Hello, Holberton', 10, 0 ; format string to printf
           
 section .text
-    global main       ; make fn cisible to linker
-    extern printf     ; printf is external
+    global main        ; make fn cisible to linker
+    extern printf      ; printf is external
     
 main:
-    push rbp          ; save base ptr
+    push rbp           ; save base ptr
     mov rbp, rsp       ; set base ptr to stack ptr
     
-    sub rsp, 8        ; allocate space for variable
-    lea rax, [hi]     ; load effective address to a random register
-    xor eax, eax      ; set eax register to 0
-    call printf       ; call function
+    sub rsp, 8         ; allocate space for variable
+    lea rdi, [hi]      ; load effective address to a random register
+    xor eax, eax       ; set eax register to 0
+    call printf        ; call function
     
-    add rsp, 8        ; free memory
-    mov rax, 0        ; return 0
-    pop rbp           ; restore base ptr
-    ret               ; return to os
+    add rsp, 8         ; free memory
+    xor eax, eax       ; return 0
+    pop rbp            ; restore base ptr
+    ret                ; return to os
