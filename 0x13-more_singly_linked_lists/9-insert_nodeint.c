d798c206d71dd381e6d3616cca209bcb65d09763
overwatch <nzioki.grace@gmail.com>
2023-05-01 13:06:07 +0300

    insert node at index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,48 +1,45 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - inserts node at given position
- * @head: double pointer to the first node
+ * insert_nodeint_at_index - inserts node at index
+ * @head: pointer to the list
  * @idx: the position to add
- * @n: the element value of node being inserted
+ * @n: the value of node being inserted
  * Return: pointer to node inserted
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i = 1;
-	listint_t *add_me, *current, *temp;
+	listint_t *add_me, *current;
 
 	add_me = malloc(sizeof(listint_t));
 
 	if (!add_me)
 		return (NULL);
 
+
+	add_me->n = n;
+	/* if we're inserting at the head */
+	if (idx == 0)
+	{
+		add_me->next = *head;
+		*head = add_me;
+	}
 	else
 	{
-		add_me->n = n;
-		/* if we're inserting at the head */
-		if (idx == 0)
+		current = *head;
+		while (i < idx)
 		{
-			add_me->next = *head;
-			*head = add_me;
+			if (!current) /* the end */
+				return (NULL);
+			current = current->next;
+			i++;
 		}
-		else
-		{
-			current = *head;
-			while (i < idx)
-			{
-				if (!current || !current->next) /* the end */
-					return (NULL);
-				current = current->next;
-				i++;
-			}
-			/* when we reach the desired location */
-			temp = current->next; /* temp holds i+1 */
-			add_me->next = temp; /* addme points i as next */
-			current->next = add_me;
-			/* ==> current --> add_me --> current.next */
-			return (add_me);
-		}
+		/* when we reach the desired location */
+		add_me->next = current->next;
+		current->next = add_me;
+		/* current --> add_me --> current.next */
+		return (add_me);
 	}
 	/* if we get here, then something didn't go well */
 	return (NULL);
