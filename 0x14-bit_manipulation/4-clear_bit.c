94a0a48385899ae5600da6d0f8756bdbcd94c2f5
overwatch <nzioki.grace@gmail.com>
2023-05-05 00:06:55 +0300

    flip at index

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,75 +1,25 @@
 #include "main.h"
 
 /**
- * _pow - gets the value of base raised to power exponent
- * @base: number to raise
- * @exponent: raise by
- * Return: the power
- */
-unsigned int _pow(int base, int exponent)
-{
-	unsigned int product = 1;
-
-	if (exponent == 0)
-		return (1);
-
-	while (exponent > 0)
-	{
-		product *= base;
-		exponent--;
-	}
-	return (product);
-}
-
-/**
- * find_power - gets the log of num just less than given number
- * @num: the upper limit
- *
- * Return: indice
- */
-unsigned long int find_power(unsigned long int num)
-{
-	unsigned long int power = 0, x = 1;
-
-	while (1)
-	{
-		if (x * 2 > num)
-			break;
-		x *= 2;
-		power++;
-	}
-	return (power);
-}
-
-/**
  * clear_bit - sets value at index to 0
- * @n: the array holding the binary number
+ * @n: the actual number in decimal
  * @index: the position in consideration
  *
  * Return: 1 on sucees else -1
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int e, num = *n;
+	unsigned int dec = 1, i;
 
-	if (!n)
+	for (i = 0; i < index; i++)
+		dec *= 2;
+
+	/* error */
+	if (dec > *n)
 		return (-1);
 
-	while (num > 0)
-	{
-		e = find_power(num);
-		if (e == index) /* bit at given index is 1 */
-		{
-			*n -= _pow(2, e);
-			return (1);
-		}
-		num -= _pow(2, e);
-	}
-
-	if (num == 0) /* something's not right */
-		return (1);
-	else
-		return (-1);
+	*n -= dec;
+	return (1);
 }
 
 
