21bad25e9efe5076daeff6f3916fa521e3c394a8
Grace <nzioki.grace@gmail.com>
2023-07-06 21:38:16 +0300

    convert from binary to base 10

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * binary_to_unit - converts binary number given as a string to a decimal
+ * b: immutable string representation of binary number 
+ *
+ * Return: the number in decimal
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int last = strlen(b) - 1;
+	int e, idx;
+	int exponent = 0, result = 0, value;
+	char cha;
+
+	for (idx = last; idx >= 0; idx--)
+	{
+		cha = *(b + idx);
+		if (cha == '1')
+		{
+			value = 1;
+			for (e = 0; e < exponent; e++)
+				value *= 2;
+			result += value;
+		}
+		else if (cha != '0')
+			return (0);
+		exponent += 1;
+	}
+	return (result);
+}
