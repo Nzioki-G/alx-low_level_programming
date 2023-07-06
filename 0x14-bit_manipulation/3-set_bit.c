405a84db9936d139be566b99d0939e9437f47cf6
Grace <nzioki.grace@gmail.com>
2023-07-06 23:43:43 +0300

    set to 1 at index

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * set_bit - sets value of bit at a given index to 1
+ * @n: the reference of a base-10 number
+ * @index: the index in n as base-2
+ *
+ * Return: 1 on success, -1 on error
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int bit, value, i;
+
+	if (*n >> index || *n == 0)
+	{
+		bit = *n >> index;
+		if ((bit & 1) == 0)
+		{
+			value = 1;
+			for (i = 0; i < index; i++)
+				value *= 2;
+			*n += value;
+		}
+		return (*n);
+	}
+	return (-1);
+}
