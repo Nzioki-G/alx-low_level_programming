ed24cf5f4fd5e81845bb2f0812ca6ba969695346
Grace <nzioki.grace@gmail.com>
2023-07-10 20:48:12 +0300

    append to file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text to file the end of file
+ * @filename: name of the file to append @text_content
+ * @text_content: text to append to @filename
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	ssize_t ok;
+	int fd, bytes;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		bytes = strlen(text_content);
+		ok = write(fd, text_content, bytes);
+		if (ok < bytes)
+			return (-1);
+	}
+
+	ok = close(fd);
+	if (ok == -1)
+		return (-1);
+
+	return (1);	
+}
