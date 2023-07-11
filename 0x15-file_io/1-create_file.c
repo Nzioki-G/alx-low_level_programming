63f0cb1483c7188908dea000417d3e7717c64c5e
Grace <nzioki.grace@gmail.com>
2023-07-11 11:28:56 +0300

    create/open file, trunc if necessary, write into it

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	/* open the file, parameters: name, flags, mode */
-	fd = open(filename, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 
