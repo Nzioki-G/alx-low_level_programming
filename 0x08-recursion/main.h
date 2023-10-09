328856e4a515cf6468ea023c3814fc1a14bb3bb2
Grace <nzioki.grace@gmail.com>
2023-10-09 11:10:55 +0300

    header

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,7 +8,7 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
-void sqrt_helper(int num, int potentialRoot);
+double sqrt_helper(double num, double potentialRoot);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int palindrome_helper(char *s, int last_index);
